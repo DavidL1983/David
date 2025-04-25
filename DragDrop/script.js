e3a3dfda7968c61fe1dd1c3fabd93a42c1b1c84e
DavidL1983 <dwlaidlaw@gmail.com>
2025-04-25 21:12:14 +0000

    Does this work?

--- DragDrop/script.js
+++ DragDrop/script.js
@@ -3,6 +3,113 @@
 // https://youtu.be/lSCLnWoa6Tw
 
 
+// Part 1: Drag & drop files
+
+document.addEventListener("DOMContentLoaded", event => {
+    const fileDropzone = document.getElementById("file-dropzone");
+    const output = document.getElementById("output");
+  
+    if (window.FileList && window.File) {
+      fileDropzone.addEventListener("dragover", event => {
+        event.stopPropagation();
+        event.preventDefault();
+        event.dataTransfer.dropEffect = "copy";
+        fileDropzone.classList.add("dragover");
+      });
+  
+      fileDropzone.addEventListener("dragleave", event => {
+        fileDropzone.classList.remove("dragover");
+      });
+  
+      fileDropzone.addEventListener("drop", event => {
+        fileDropzone.classList.remove("dragover");
+        event.stopPropagation();
+        event.preventDefault();
+  
+        for (const file of event.dataTransfer.files) {
+          const name = file.name;
+          const size = file.size ? Math.round(file.size / 1000) : 0;
+  
+          if (file.type && file.type.startsWith("image/")) {
+              const li = document.createElement("li");
+              li.textContent = name + " (" + size + " KB)";
+              output.appendChild(li);
+          }
+        }
+      }); 
+    }
+  });
+  
+  
+  
+  // Part 2: Drag & drop elements
+  
+  var draggedElement = null;
+  var items;
+  
+  function handleDragStart(e) {
+      this.style.opacity = "0.4";
+      draggedElement = this;
+  
+      e.dataTransfer.effectAllowed = "move";
+      e.dataTransfer.setData("item", this.innerHTML);
+  }
+  
+  function handleDragOver(e) {
+      if (e.preventDefault) 
+          e.preventDefault();
+  
+      e.dataTransfer.dropEffect = "move";
+      return false;
+  }
+  
+  function handleDragEnter(e) {
+      this.classList.add("dragover");
+  }
+  
+  function handleDragLeave(e) {
+      this.classList.remove("dragover");
+  }
+  
+  function handleDrop(e) {
+      if (e.stopPropagation)
+          e.stopPropagation();
+  
+      if (draggedElement != this) {
+          draggedElement.innerHTML = this.innerHTML;
+          draggedElement.setAttribute("data-item", this.innerHTML);
+  
+          let replacedItem = e.dataTransfer.getData("item");
+          this.innerHTML = replacedItem;
+          this.setAttribute("data-item", replacedItem);
+      }
+  
+      return false;
+  }
+  
+  function handleDragEnd(e) {
+      this.style.opacity = "1";
+  
+      items.forEach(function(item) {
+          item.classList.remove("dragover");
+      });
+  }
+  
+  document.addEventListener("DOMContentLoaded", event => {
+      items = document.querySelectorAll(".container .box");
+  
+      items.forEach(function(item) {
+        item.addEventListener("dragstart", handleDragStart);
+        item.addEventListener("dragenter", handleDragEnter);
+        item.addEventListener("dragover", handleDragOver);
+        item.addEventListener("dragleave", handleDragLeave);
+        item.addEventListener("drop", handleDrop);
+        item.addEventListener("dragend", handleDragEnd);
+      });
+  });
+  
+  
+  
   // Part 3: Using a plugin
   
   function dragMoveListener(event) {
